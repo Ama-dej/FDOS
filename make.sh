6ccca1ce4806c235af690f3da09eaa0f292d4b0a
Ama-dej <amadej.arh@gmail.com>
2023-04-22 11:45:27 +0200

    bobri

--- make.sh
+++ make.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 rm bin/* >> /dev/null
+rm img/* >> /dev/null
 
 nasm -f bin src/boot.asm -o bin/boot.bin
 nasm -f bin src/dos.asm -o bin/dos.bin
 
-dd if=/dev/zero of=bin/floppy1440.img count=2880 bs=512
-mkfs.fat -F 12 bin/floppy1440.img
+dd if=/dev/zero of=img/floppy1440.img count=2880 bs=512
+mkfs.fat -F 12 img/floppy1440.img
 
-dd if=bin/boot.bin of=bin/floppy1440.img conv=notrunc
-mcopy -i bin/floppy1440.img bin/dos.bin "::DOS.SYS"
+dd if=bin/boot.bin of=img/floppy1440.img conv=notrunc
+mcopy -i img/floppy1440.img bin/dos.bin "::DOS.SYS"
