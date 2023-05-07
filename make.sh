63946b5215292ff5929c7c25cac70ba4794f4655
Ama-dej <amadej.arh@gmail.com>
2023-05-07 10:28:14 +0200

    napol funkcionalna write prekinitev

--- make.sh
+++ make.sh
@@ -8,6 +8,7 @@ nasm -f bin src/dos.asm -o bin/dos.bin
 
 nasm -f bin prg/snake.asm -o bin/snake.bin
 nasm -f bin prg/big.asm -o bin/big.bin
+nasm -f bin prg/test.asm -o bin/test.bin
 
 dd if=/dev/zero of=img/floppy1440.img count=2880 bs=512
 mkfs.fat -F 12 -s 2 img/floppy1440.img
@@ -16,4 +17,5 @@ dd if=bin/boot.bin of=img/floppy1440.img conv=notrunc
 mcopy -i img/floppy1440.img bin/dos.bin "::DOS.SYS"
 
 mcopy -i img/floppy1440.img bin/big.bin "::big.bin"
-mcopy -i img/floppy1440.img bin/snake.bin "::snakeeee.bin"
+mcopy -i img/floppy1440.img bin/snake.bin "::snake.bin"
+mcopy -i img/floppy1440.img bin/test.bin "::test.bin"
