15258c1f22e071d349789d934c54ced6ca87e132
Ama-dej <amadej.arh@gmail.com>
2023-05-07 15:52:18 +0200

    kaj pri svetem BIOSu!!?!?!?!?!

--- make.sh
+++ make.sh
@@ -7,6 +7,7 @@ nasm -f bin src/boot.asm -o bin/boot.bin
 nasm -f bin src/dos.asm -o bin/dos.bin
 
 nasm -f bin prg/snake.asm -o bin/snake.bin
+nasm -f bin prg/tetris.asm -o bin/tetris.bin
 nasm -f bin prg/big.asm -o bin/big.bin
 nasm -f bin prg/test.asm -o bin/test.bin
 
@@ -16,6 +17,7 @@ mkfs.fat -F 12 -s 2 img/floppy1440.img
 dd if=bin/boot.bin of=img/floppy1440.img conv=notrunc
 mcopy -i img/floppy1440.img bin/dos.bin "::DOS.SYS"
 
-mcopy -i img/floppy1440.img bin/big.bin "::big.bin"
 mcopy -i img/floppy1440.img bin/snake.bin "::snake.bin"
+mcopy -i img/floppy1440.img bin/tetris.bin "::tetris.bin"
+mcopy -i img/floppy1440.img bin/big.bin "::big.bin"
 mcopy -i img/floppy1440.img bin/test.bin "::test.bin"
