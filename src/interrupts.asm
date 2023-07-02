9ae7a8bfb3d15bb57b35faaeb35780c5e4188107
Ama-dej <amadej.arh@gmail.com>
2023-07-02 16:33:50 +0200

    reorganizacija

---
+++ src/interrupts.asm
@@ -0,0 +1,737 @@
+; FDOS INTERRUPT (INT 0x80)
+; -------------------------
+
+DOS_INT:
+        PUSHA
+
+        PUSH BX
+        PUSH DS
+
+        MOV BX, DOS_SEGMENT
+        MOV DS, BX
+
+        MOV BYTE[INT_RET_CODE], 0x00
+
+        MOVZX BX, AH
+        SHL BX, 1
+        ADD BX, INT_JUMP_TABLE
+        MOV AX, WORD[BX]
+
+        POP DS
+        POP BX
+
+        JMP AX 
+
+; AH = 0x00
+; Returns from the program to 16-DOS.
+EXIT_INT:
+        POPA
+
+        MOV AX, DOS_SEGMENT
+        MOV DS, AX
+
+        XOR AX, AX
+        MOV SS, AX
+        MOV SP, 0x7E00
+
+        JMP DOS_SEGMENT:DOS_START
+
+; AH = 0x01
+; SI = Pointer to string.
+; CX = Number of bytes to print.
+PRINT_INT:
+        CLD
+        MOV AH, 0x0E
+
+.PRINT_LOOP:
+        LODSB
+        INT 0x10
+        LOOP .PRINT_LOOP
+
+        JMP RET_INT
+
+; AH = 0x02
+; SI = Pointer to buffer.
+; CX = Maximum number of bytes to get from the user.
+; Scan terminates when the enter key is pressed.
+SCAN_INT:
+        MOV DX, SI
+        INC CX
+
+.SCAN_LOOP:
+        MOV AH, 0x00
+        INT 0x16
+
+        CMP AL, 0x0D
+        JE RET_INT
+
+        CMP AL, 0x08
+        JE .BACKSPACE_PRESSED
+
+        TEST CX, CX
+        JZ .SCAN_LOOP
+
+        MOV AH, 0x0E
+        INT 0x10
+
+        MOV BYTE[SI], AL
+        INC SI
+        LOOP .SCAN_LOOP
+
+.BACKSPACE_PRESSED:
+        CMP SI, DX
+        JE .SCAN_LOOP
+
+        PUSHA
+        MOV AH, 0x03
+        MOV BH, 0
+        INT 0x10
+
+        CMP DL, 0
+        JNZ .MOVE_NORMAL
+
+        MOV DL, 80
+        DEC DH
+
+.MOVE_NORMAL:
+        MOV AH, 0x02
+        DEC DL
+        INT 0x10
+
+        MOV AH, 0x0A
+        MOV AL, ' '
+        MOV BL, 7
+        MOV CX, 1
+        INT 0x10
+        POPA
+
+        DEC SI
+        MOV BYTE[SI], 0x00
+        INC CX
+        JMP .SCAN_LOOP
+
+; AH = 0x03
+; DX = Value to print.
+PRINTI_INT:
+        MOV AX, DX
+        MOV BX, 10
+        XOR CX, CX
+
+.DIV_LOOP:
+        XOR DX, DX
+        DIV BX
+
+        ADD DX, 48
+        PUSH DX
+
+        INC CX
+        TEST AX, AX
+        JNZ .DIV_LOOP
+
+.PRINT_LOOP:
+        POP AX
+        MOV AH, 0x0E
+        INT 0x10
+        LOOP .PRINT_LOOP
+
+        JMP RET_INT
+
+INT_FILENAME_BUFFER: TIMES 11 DB ' '
+
+; AH = 0x04
+; ES:BX = Destination buffer.
+; SI = Pointer to filename.
+; CX = Number of bytes to read.
+; DX/DI = Byte offset
+; DX = Lower part of offset.
+; DI = Higher part of offset.
+READFILE_INT:
+        PUSH DS
+        PUSH ES
+        PUSH BX
+        PUSH DI
+
+        MOV AX, DOS_SEGMENT
+        MOV ES, AX
+        MOV DI, INT_FILENAME_BUFFER
+        CALL CONVERT_TO_8_3
+        JC .LABEL_HACK ; Evil hack to save a few instructions.
+
+        MOV DS, AX
+        MOV SI, INT_FILENAME_BUFFER
+        MOV DI, BX
+
+        XOR BX, BX
+        MOV ES, BX
+        MOV BX, WORD[CURRENT_DIRECTORY]
+        CALL FIND_ENTRY
+
+        MOV SI, DOS_SEGMENT
+        MOV DS, SI
+
+        MOV AX, WORD[ES:BX + 28]
+        MOV WORD[FILE_SIZE_LOWER], AX
+        MOV AX, WORD[ES:BX + 30]
+        MOV WORD[FILE_SIZE_UPPER], AX
+
+        MOV AX, WORD[ES:BX + 26]
+
+.LABEL_HACK:
+        POP DI
+        POP BX
+        POP ES
+        JC .NOT_FOUND
+
+        PUSH ES
+
+        CMP DI, WORD[FILE_SIZE_UPPER]
+        JA .READ_ERROR
+        JB .SUBTRACT
+
+        CMP DX, WORD[FILE_SIZE_LOWER]
+        JA .READ_ERROR 
+
+.SUBTRACT:
+        SUB WORD[FILE_SIZE_UPPER], DI
+        SUB WORD[FILE_SIZE_LOWER], DX
+        SBB WORD[FILE_SIZE_UPPER], 0
+
+        PUSH BX
+        SHR BX, 4
+        AND BX, 0xFFF0
+        MOV SI, ES
+        ADD SI, BX
+        DEC SI
+        MOV ES, SI
+        POP BX
+        AND BX, 0x00FF
+        ADD BX, 16
+
+        CALL RESET_DISK
+
+.GET_TO_CLUSTER_OFFSET:
+        CMP DI, 0
+        JNZ .SKIP
+
+        CMP DX, WORD[BYTES_PER_CLUSTER] 
+        JB .READ_LOOP
+
+.SKIP:
+        SUB DX, WORD[BYTES_PER_CLUSTER]
+        SBB DI, 0
+
+        CALL GET_NEXT_CLUSTER
+        JMP .GET_TO_CLUSTER_OFFSET
+
+.READ_LOOP:
+        PUSH ES
+        PUSH BX
+
+        MOV BX, DOS_SEGMENT
+        MOV ES, BX
+        MOV BX, DATA_BUFFER
+
+        CALL READ_DATA
+        POP BX
+        POP ES
+        JC .READ_ERROR
+
+        CALL GET_NEXT_CLUSTER
+
+        PUSH CX
+        PUSH DI
+
+        MOV DI, WORD[BYTES_PER_CLUSTER]
+
+        CMP AX, 0xFF8
+        JL .PROCEED
+
+        CMP CX, WORD[FILE_SIZE_LOWER]
+        JB .COPY_DATA
+
+        MOV CX, WORD[FILE_SIZE_LOWER]
+        JMP .COPY_DATA
+
+.PROCEED:
+        MOV CX, DI
+        SUB CX, DX
+        SUB WORD[FILE_SIZE_LOWER], CX
+        SBB WORD[FILE_SIZE_UPPER], 0
+
+.COPY_DATA:
+        MOV SI, DATA_BUFFER
+        ADD SI, DX
+        MOV DI, BX
+        CALL MEMCPY
+
+        POP DI
+        POP CX
+
+        MOV SI, CX
+        ADD CX, DX
+        ADD SI, DX
+        JC .OVERFLOW
+
+        CMP CX, WORD[BYTES_PER_CLUSTER]
+        JBE .OUT
+
+.OVERFLOW:
+        SUB CX, WORD[BYTES_PER_CLUSTER]
+
+        MOV DI, WORD[BYTES_PER_CLUSTER]
+        SHR DI, 4
+
+        PUSH DX
+        SHR DX, 4
+
+        MOV SI, ES
+        ADD SI, DI
+        SUB SI, DX
+        MOV ES, SI
+        POP DX
+
+        AND DX, 0x000F
+        SUB BX, DX
+
+        CMP AX, 0xFF8
+        JL .READ_LOOP
+
+.OUT:
+        POP ES
+        POP DS
+        JMP RET_CODE_INT
+
+.NOT_FOUND:
+        MOV BYTE[INT_RET_CODE], 0x01
+        POP DS
+        JMP RET_CODE_INT
+
+.READ_ERROR:
+        MOV BYTE[INT_RET_CODE], 0x02
+        POP ES
+        POP DS
+        JMP RET_CODE_INT
+
+FILE_SIZE_LOWER: DW 0
+FILE_SIZE_UPPER: DW 0
+
+; AH = 0x05
+; ES:BX = Buffer to write.
+; SI = Pointer to file entry.
+; CX = Number of bytes to write.
+; DX/DI = Byte offset
+; DX = Lower part of offset.
+; DI = Higher part of offset.
+WRITEFILE_INT:
+        PUSH DS
+        PUSH ES
+        PUSH BX
+        PUSH DI
+
+        MOV AX, DOS_SEGMENT
+        MOV ES, AX
+        MOV DI, INT_FILENAME_BUFFER
+        CALL CONVERT_TO_8_3
+        JC .LABEL_HACK ; Evil hack to save a few instructions.
+
+        MOV DS, AX
+        MOV SI, INT_FILENAME_BUFFER
+        MOV DI, BX
+
+        XOR BX, BX
+        MOV ES, BX
+        MOV BX, WORD[CURRENT_DIRECTORY]
+        CALL FIND_ENTRY
+
+        MOV SI, DOS_SEGMENT
+        MOV DS, SI
+
+        MOV AX, WORD[ES:BX + 28]
+        MOV WORD[FILE_SIZE_LOWER], AX
+        MOV AX, WORD[ES:BX + 30]
+        MOV WORD[FILE_SIZE_UPPER], AX
+
+        MOV AX, WORD[ES:BX + 26]
+        MOV WORD[INT_TEMP], BX
+
+.LABEL_HACK:
+        POP DI
+        POP BX
+        POP ES
+        JC .NOT_FOUND
+        PUSH ES
+
+        PUSH BX
+        SHR BX, 4
+        AND BX, 0xFFF0
+        MOV SI, ES
+        ADD SI, BX
+        DEC SI
+        MOV ES, SI
+        POP BX
+        AND BX, 0x00FF
+        ADD BX, 16
+
+        ; PUSHA
+        ; MOV AH, 0x03
+        ; MOV DX, CX
+        ; INT 0x80
+        ; CALL NLCR
+        ; POPA
+
+        CMP DI, WORD[FILE_SIZE_UPPER]
+        JA .WRITE_ERROR
+        JB .CONTINUE
+
+        CMP DX, WORD[FILE_SIZE_LOWER]
+        JA .WRITE_ERROR 
+
+.CONTINUE:
+        MOV WORD[FILE_SIZE_LOWER], DX
+        MOV WORD[FILE_SIZE_UPPER], DI
+
+        ; PUSH ES
+        ; PUSH DX
+        ; PUSH DI
+
+        ; XOR SI, SI
+        ; MOV ES, SI
+        ; MOV SI, WORD[INT_TEMP]
+
+        ; ADD DX, CX
+        ; ADC DI, 0
+
+        ; CMP DI, WORD[ES:SI + 30]
+        ; JB .POP
+        ; JA .STORE
+
+        ; CMP DX, WORD[ES:SI + 28]
+        ; JB .POP
+
+; .STORE:
+        ; MOV WORD[ES:SI + 30], DI
+        ; MOV WORD[ES:SI + 28], DX
+
+; .POP:
+        ; POP DI
+        ; POP DX
+        ; POP ES
+
+        CALL RESET_DISK
+
+        CMP AX, 0
+        JNZ .GET_TO_STARTING_CLUSTER
+
+        PUSH ES
+        PUSH DX
+        PUSH SI
+
+        XOR SI, SI
+        MOV ES, SI
+        MOV SI, WORD[INT_TEMP]
+
+        CALL GET_FREE_CLUSTER
+        MOV WORD[ES:SI + 26], AX
+
+        MOV DX, 0xFFF
+        CALL WRITE_CLUSTER
+
+        POP SI
+        POP DX
+        POP ES
+
+.GET_TO_STARTING_CLUSTER:
+        CMP DI, 0
+        JNZ .SKIP
+
+.SKIP:
+        CMP DX, WORD[BYTES_PER_CLUSTER]
+        JB .WRITE_LOOP
+
+        SUB DX, WORD[BYTES_PER_CLUSTER]
+        SBB DI, 0
+
+        MOV WORD[INT_WRITE_LAST], AX
+        CALL GET_NEXT_CLUSTER
+        JMP .GET_TO_STARTING_CLUSTER
+
+.WRITE_LOOP:
+        CMP AX, 0xFF8
+        JL .OK
+
+        PUSH DX
+        CALL GET_FREE_CLUSTER
+        JC .OUT_OF_SPACE_ERROR
+
+        MOV DX, 0xFFF
+        CALL WRITE_CLUSTER
+
+        MOV DX, AX
+        MOV AX, WORD[INT_WRITE_LAST]
+        CALL WRITE_CLUSTER
+
+        MOV AX, DX
+
+        POP DX
+
+.OK:
+        PUSH CX
+        PUSH ES
+        PUSH BX
+        PUSH BX
+        PUSH ES
+
+        MOV BX, DOS_SEGMENT
+        MOV ES, BX
+        MOV BX, DATA_BUFFER
+
+        MOV SI, CX
+        ADD SI, DX
+        JC .OVERFLOW ; This is here in case CX is a really high value and would therefore break this algorithm (eg. 0xFFFF + 0x200 = 0x1FF, which is smaller than the amount of bytes per cluster).
+
+        PUSH CX
+        ADD CX, DX
+        CMP CX, WORD[BYTES_PER_CLUSTER]
+        POP CX
+        JB .READ_DATA
+
+.OVERFLOW:
+        MOV CX, WORD[BYTES_PER_CLUSTER]
+
+        CMP DX, 0
+        JZ .COPY_DATA
+
+        SUB CX, DX
+
+.READ_DATA:
+        ; PUSH AX
+        ; MOV AL, '*'
+        ; CALL PUTCHAR
+        ; POP AX
+
+        CALL READ_DATA
+        JNC .COPY_DATA
+
+        POP ES
+        POP BX
+        POP BX
+        POP ES
+        POP CX
+        JMP .WRITE_ERROR
+
+.COPY_DATA:
+        POP DS
+        POP SI
+        MOV DI, DATA_BUFFER
+        ADD DI, DX
+        CALL MEMCPY
+
+        MOV SI, DOS_SEGMENT
+        MOV DS, SI
+
+        ; PUSHA
+        ; MOV AH, 0x03
+        ; MOV DX, CX
+        ; INT 0x80
+        ; CALL NLCR
+        ; POPA
+
+        CALL WRITE_DATA
+        JC .WRITE_ERROR
+
+        ADD WORD[FILE_SIZE_LOWER], CX
+        ADC WORD[FILE_SIZE_UPPER], 0
+
+        POP BX
+        POP ES
+        POP CX
+
+        MOV SI, CX
+        ADD CX, DX
+        ADD SI, DX
+        JC .OVERFLOW_AGAIN
+
+        CMP CX, WORD[BYTES_PER_CLUSTER]
+        JBE .OUT
+
+.OVERFLOW_AGAIN:
+        SUB CX, WORD[BYTES_PER_CLUSTER]
+
+        MOV DI, WORD[BYTES_PER_CLUSTER]
+        SHR DI, 4
+
+        PUSH DX
+        SHR DX, 4
+
+        MOV SI, ES
+        ADD SI, DI
+        SUB SI, DX
+        MOV ES, SI 
+        POP DX
+
+        AND DX, 0x000F
+        SUB BX, DX
+        XOR DX, DX
+
+        MOV WORD[INT_WRITE_LAST], AX
+        CALL GET_NEXT_CLUSTER
+
+        JMP .WRITE_LOOP
+
+.OUT:
+        POP ES
+        JMP RET_WRITE_INT
+
+.NOT_FOUND:
+        MOV BYTE[INT_RET_CODE], 0x01
+        JMP RET_WRITE_INT
+
+.WRITE_ERROR:
+        MOV BYTE[INT_RET_CODE], 0x02
+        POP ES
+        JMP RET_WRITE_INT
+
+.OUT_OF_SPACE_ERROR:
+        MOV BYTE[INT_RET_CODE], 0x03
+        POP DX
+        POP ES
+        JMP RET_WRITE_INT
+
+INT_WRITE_LAST: DW 0
+
+; AH = 0x06
+; DI = Pointer to 80 byte buffer.
+COMMAND_PARAMETERS_INT:
+        PUSH ES
+
+        MOV SI, DS
+        MOV ES, SI
+
+        MOV SI, DOS_SEGMENT
+        MOV DS, SI
+        MOV SI, COMMAND_PARSED
+
+        MOV CX, 80
+        CALL MEMCPY
+
+        MOV SI, ES
+        MOV DS, SI
+
+        POP ES
+        JMP RET_INT
+
+; AH = 0x07
+; CX = Number of milliseconds to sleep.
+SLEEP_INT:
+        PUSH DS
+
+        CLI
+
+        XOR BX, BX
+        MOV DS, BX
+        MOV BX, 0x08 * 4
+
+	MOV DX, WORD[DS:BX] ; Save the default values set by the BIOS.
+	MOV DI, WORD[DS:BX + 2] ; Some BIOS interrupts get borked if you don't reset these values.
+
+        MOV WORD[DS:BX], PIT_SLEEP_INTERRUPT
+
+        MOV AX, DOS_SEGMENT
+        MOV WORD[DS:BX + 2], AX
+
+        MOV AL, 0b00110100
+        OUT 0x43, AL
+        MOV AL, 0xA9
+        OUT 0x40, AL
+        MOV AL, 0x04
+        OUT 0x40, AL
+        STI
+
+.WAIT:
+        TEST CX, CX
+        JZ .OUT
+        HLT
+        JMP .WAIT
+
+.OUT:
+        CLI
+        MOV AL, 0b00110100
+        OUT 0x43, AL
+        XOR AL, AL
+        OUT 0x40, AL
+        OUT 0x40, AL
+
+        XOR AX, AX
+        MOV DS, AX
+	MOV WORD[DS:BX], DX
+	MOV WORD[DS:BX + 2], DI
+        STI
+
+        POP DS
+        JMP RET_INT
+
+PIT_SLEEP_INTERRUPT:
+        DEC CX
+
+        MOV AL, 0x20
+        OUT 0x20, AL
+        IRET
+
+RET_WRITE_INT:
+        PUSH ES
+
+        XOR SI, SI
+        MOV ES, SI
+        MOV SI, WORD[INT_TEMP]
+
+        MOV DX, WORD[FILE_SIZE_LOWER]
+        MOV DI, WORD[FILE_SIZE_UPPER]
+
+        CMP DI, WORD[ES:SI + 30]
+        JB .POP
+        JA .STORE
+
+        CMP DX, WORD[ES:SI + 28]
+        JB .POP
+
+.STORE:
+        MOV WORD[ES:SI + 30], DI
+        MOV WORD[ES:SI + 28], DX
+
+.POP:
+        POP ES
+
+        CALL UPDATE_FS
+        POP DS
+        JMP RET_CODE_INT
+
+RET_CODE_INT:
+        POPA
+        PUSH BX
+        PUSH DS 
+        MOV BX, DOS_SEGMENT
+        MOV DS, BX
+        MOV AL, BYTE[INT_RET_CODE]
+        POP DS
+        POP BX
+        IRET
+
+RET_INT:
+        POPA
+        IRET
+
+INT_RET_CODE: DB 0
+INT_TEMP: DW 0
+INT_TEMP_JUNIOR: DW 0
+
+INT_JUMP_TABLE:
+EXIT_INT_ADDRESS: DW EXIT_INT
+PRINT_INT_ADDRESS: DW PRINT_INT
+SCAN_INT_ADDRESS: DW SCAN_INT
+PRINTI_INT_ADDRESS: DW PRINTI_INT
+READFILE_INT_ADDRESS: DW READFILE_INT
+WRITEFILE_INT_ADDRESS: DW WRITEFILE_INT ; <- Zamenjaj z WRITEFILE_INT, ko bo prekinitev varna.
+COMMAND_PARAMETERS_INT_ADDRESS: DW COMMAND_PARAMETERS_INT
+SLEEP_INT_ADDRESS: DW SLEEP_INT
+RETURN_FROM_INT_ADDRESS: TIMES 256 - ((RETURN_FROM_INT_ADDRESS - INT_JUMP_TABLE) / 2) DW RET_INT
+INT_JUMP_TABLE_END:
---
+++ src/procedures.asm
@@ -0,0 +1,713 @@
+; PROCEDURES
+; ----------
+
+; Turns on the PC speaker.
+SPK_ON:
+        PUSH AX
+        IN AL, 0x61
+        OR AL, 3
+        OUT 0x61, AL
+        POP AX
+        RET
+
+; Turns off the PC speaker.
+SPK_OFF:
+        PUSH AX
+        IN AL, 0x61
+        AND AL, 0xFC
+        OUT 0x61, AL
+        POP AX
+        RET
+
+; BL <- Value to print.
+PUTH8:
+        PUSH AX
+        PUSH CX
+        MOV AH, 0x0E
+        MOV CX, 2
+
+.LOOP:
+        ROL BL, 4
+        MOV AL, BL
+        AND AL, 0x0F
+
+        CMP AL, 10
+        SBB AL, 0x69
+        DAS
+
+        INT 0x10
+
+        DEC CX
+        JNZ .LOOP
+
+        POP CX
+        POP AX
+        RET
+
+; AL <- Character.
+;
+; AL -> Character converted to uppercase.
+TO_UPPER:
+        CMP AL, 'a'
+        JL .OUT
+
+        CMP AL, 'z'
+        JG .OUT
+
+        SUB AL, 32
+
+.OUT:
+        RET
+
+UPDATE_FS:
+        CALL STORE_FAT
+
+        XOR BX, BX
+        MOV ES, BX
+        MOV BX, WORD[CURRENT_DIRECTORY]
+
+        XOR DX, DX
+
+        MOVZX AX, BYTE[NUMBER_OF_FAT]
+        MUL WORD[SECTORS_PER_FAT]
+        ADD AX, WORD[RESERVED_SECTORS]
+
+        MOV DX, WORD[ROOT_ENTRIES]
+        ADD DX, 15
+        SHR DX, 4
+
+        MOV CL, DL
+        MOV DL, BYTE[DRIVE_NUMBER]
+
+        CALL WRITE_DISK
+
+        RET
+
+STORE_FAT:
+        PUSHA
+        PUSH ES
+
+        MOV CX, WORD[SECTORS_PER_FAT]
+
+        XOR BX, BX
+        MOV ES, BX
+        MOV BX, FILESYSTEM
+
+        MOV AX, WORD[RESERVED_SECTORS]
+        MOV DL, BYTE[DRIVE_NUMBER]
+        MOV DH, BYTE[NUMBER_OF_FAT]
+
+.STORE_LOOP:
+        CALL WRITE_DISK
+
+        ADD AX, CX
+
+        DEC DH
+        JNZ .STORE_LOOP
+
+        POP ES
+        POPA
+        RET
+
+; SI <- Filename to convert.
+; ES:DI <- Pointer to where to store the string.
+;
+; CF -> Cleared if successful.
+CONVERT_TO_8_3:
+        PUSHA
+
+        MOV AL, ' '
+        MOV CX, 11
+        CALL MEMSET
+
+        MOV BX, DI
+
+        CLC
+        MOV CX, 8
+
+.LOOP:
+        LODSB
+
+        CMP AL, '.'
+        JE .DOT
+
+        CMP AL, 0x00
+        JE .OUT
+
+        CMP CX, 0
+        JNZ .STORE_BYTE
+
+        STC
+        JMP .OUT
+
+.STORE_BYTE:
+        STOSB
+        DEC CX
+        JMP .LOOP
+
+.DOT:
+        MOV DI, BX
+        ADD DI, 8
+        MOV CX, 3
+        JMP .LOOP
+
+.OUT:
+        POPA
+        RET
+
+; ES:BX <- Pointer to file entry.
+PRINT_FILE_SIZE:
+        PUSH AX
+        PUSH CX
+        PUSH DX
+        PUSH SI
+
+        CALL GET_FILE_SIZE
+
+        MOV AH, 0x03
+        INT 0x80
+
+        MOV AL, 'K'
+        CALL PUTCHAR
+
+        POP SI
+        POP DX
+        POP CX
+        POP AX
+        RET
+
+; ES:BX <- Pointer to file entry.
+;
+; DX -> File size in KiB.
+GET_FILE_SIZE:
+        PUSH AX
+
+        MOV DX, WORD[ES:BX + 28]
+        ADD DX, 1023
+
+        MOV AX, WORD[ES:BX + 30]
+        ADC AX, 0
+        SHL AX, 6
+
+        SHR DX, 10
+        ADD DX, AX
+
+        POP AX
+        RET
+
+; Prints a new line and a carrige return.
+NLCR:
+        PUSH AX
+
+        MOV AH, 0x0E
+        MOV AL, 0x0A
+        INT 0x10
+        MOV AL, 0x0D
+        INT 0x10
+
+        POP AX
+        RET
+
+; AL <- Character to print out.
+PUTCHAR:
+        PUSH AX
+
+        MOV AH, 0x0E
+        INT 0x10
+
+        POP AX
+        RET
+
+; SI <- Pointer to first string.
+; DI <- Pointer to second string.
+;
+; ZF <- Set if strings are equal.
+STRCMP:
+        PUSH AX
+        PUSH SI
+        PUSH DI
+
+.CMP_LOOP:
+        LODSB
+        CMP BYTE[DI], AL
+        JNE .OUT
+
+        INC DI
+
+        ; CMP AL, 0x00
+        TEST AL, AL
+        JNZ .CMP_LOOP
+
+.OUT:
+        POP DI
+        POP SI
+        POP AX
+        RET
+
+; SI <- Pointer to string.
+;
+; CX -> String length.
+STRLEN:
+        PUSH SI
+        XOR CX, CX
+
+.LOOP:
+        LODSB
+        TEST AL, AL
+        JZ .OUT
+        INC CX
+        JMP .LOOP
+
+.OUT:
+        POP SI
+        RET
+
+; ES:BX <- Pointer to file entry.
+PRINT_FILENAME:
+        PUSH AX
+        PUSH BX
+        PUSH CX
+
+        MOV AH, 0x0E
+        MOV CX, 11
+
+.PRINT_LOOP:
+        MOV AL, BYTE[ES:BX]
+        INT 0x10
+
+        CMP CX, 4
+        JNE .NOT_DOT
+
+        MOV AL, '.'
+        INT 0x10
+
+.NOT_DOT:
+        INC BX
+        LOOP .PRINT_LOOP
+
+.OUT:
+        POP CX
+        POP BX
+        POP AX
+        RET
+
+; ES:BX <- Start of directory.
+; SI <- File name to find.
+;
+; CF -> Set if not found.
+; BX -> Location of entry.
+FIND_ENTRY:
+        CLC
+
+.LOOP:
+        CMP BYTE[ES:BX], 0
+        JZ .ERROR
+
+        CALL FILENAMECMP
+        JE .OUT
+
+        ADD BX, 32
+        JMP .LOOP
+
+.ERROR:
+        STC
+
+.OUT:
+        RET
+
+; ES:BX <- File name in directory entry.
+; SI <- File name to compare to.
+;
+; ZF -> Set if equal.
+FILENAMECMP:
+        PUSH AX
+        PUSH BX
+        PUSH CX
+        PUSH SI
+
+        MOV CX, 11
+
+.LOOP:
+        LODSB
+
+        CMP AL, BYTE[ES:BX]
+        JNE .OUT
+
+        INC BX
+        LOOP .LOOP
+
+        XOR AX, AX
+
+.OUT:
+        POP SI
+        POP CX
+        POP BX
+        POP AX
+        RET
+
+; AL <- Value to set to.
+; ES:DI <- Pointer to buffer.
+; CX <- Number of bytes to set.
+MEMSET:
+        PUSH CX
+        PUSH DI
+
+.LOOP:
+        TEST CX, CX
+        JZ .OUT
+        MOV BYTE[ES:DI], AL
+        INC DI 
+        LOOP .LOOP
+
+.OUT:
+        POP DI
+        POP CX
+        RET
+
+; SI <- Source buffer.
+; ES:DI <- Destination buffer.
+; CX <- Number of bytes to copy.
+MEMCPY:
+        PUSH AX
+        PUSH CX
+        PUSH SI
+        PUSH DI
+
+        TEST CX, CX
+        JZ .OUT
+
+.LOOP:
+        LODSB
+        STOSB
+        LOOP .LOOP
+
+.OUT:
+        POP DI
+        POP SI
+        POP CX
+        POP AX
+        RET
+
+; AX <- Current cluster.
+; ES:BX <- Source buffer.
+WRITE_DATA:
+        PUSH AX
+        PUSH CX
+        PUSH DX
+
+        SUB AX, 2
+        MOVZX CX, BYTE[SECTORS_PER_CLUSTER]
+        MUL CX 
+
+        ADD AX, WORD[DATA_AREA_BEGIN]
+        MOV CL, BYTE[SECTORS_PER_CLUSTER]
+        MOV DL, BYTE[DRIVE_NUMBER]
+        CALL WRITE_DISK
+
+        POP DX
+        POP CX
+        POP AX
+        RET
+
+; AX <- Current cluster.
+; ES:BX <- Destination buffer.
+READ_DATA:
+        PUSH AX
+        PUSH CX
+        PUSH DX
+
+        SUB AX, 2
+        MOVZX CX, BYTE[SECTORS_PER_CLUSTER]
+        MUL CX 
+
+        ADD AX, WORD[DATA_AREA_BEGIN]
+        MOV CL, BYTE[SECTORS_PER_CLUSTER]
+        MOV DL, BYTE[DRIVE_NUMBER]
+        CALL READ_DISK
+
+        POP DX
+        POP CX
+        POP AX
+        RET
+
+; AX <- Cluster to write to.
+; DX <- Value to write.
+WRITE_CLUSTER:
+        PUSH ES
+        PUSH BX
+        PUSH DX
+
+        MOV BX, FILESYSTEM >> 4
+        MOV ES, BX
+        MOV BX, AX
+
+        SHR BX, 1
+        ADD BX, AX
+
+        TEST AX, 1
+        JZ .EVEN_CLUSTER
+
+        SHL DX, 4
+        AND WORD[ES:BX], 0x000F
+        OR WORD[ES:BX], DX
+        JMP .ODD_CLUSTER
+
+.EVEN_CLUSTER:
+        AND WORD[ES:BX], 0xF000
+        OR WORD[ES:BX], DX
+
+.ODD_CLUSTER:
+        POP DX
+        POP BX
+        POP ES
+        RET
+
+; AX -> Free cluster location.
+GET_FREE_CLUSTER:
+        PUSH ES
+        PUSH BX
+
+        MOV AX, WORD[LAST_ALLOCATED_CLUSTER]
+        INC AX
+
+        MOV BX, FILESYSTEM >> 4
+        MOV ES, BX
+
+.SEARCH:
+        AND AX, 0xFFF
+
+        CMP AX, WORD[LAST_ALLOCATED_CLUSTER]
+        JE .OUT_OF_SPACE
+
+        MOV BX, AX
+        SHR BX, 1
+        ADD BX, AX
+
+        PUSH AX
+        MOV AX, WORD[ES:BX]
+        TEST AX, 1
+        JZ .EVEN_CLUSTER
+
+        AND AX, 0x0FFF
+        POP AX
+        JZ .OUT
+        INC AX
+        JMP .SEARCH
+
+.EVEN_CLUSTER:
+        SHR AX, 4
+        POP AX
+        JZ .OUT
+        INC AX
+        JMP .SEARCH
+
+.OUT:
+        MOV WORD[LAST_ALLOCATED_CLUSTER], AX
+
+        POP BX
+        POP ES
+        RET
+
+.OUT_OF_SPACE:
+        STC
+        POP BX
+        POP ES
+        RET
+
+LAST_ALLOCATED_CLUSTER: DW 0
+
+; AX <- Current cluster.
+;
+; AX -> Next cluster.
+GET_NEXT_CLUSTER:
+        PUSH ES
+        PUSH BX
+
+        MOV BX, FILESYSTEM >> 4
+        MOV ES, BX
+        MOV BX, AX
+        SHR BX, 1
+        ADD BX, AX
+
+        TEST AX, 1
+        MOV AX, WORD[ES:BX]
+        JZ .EVEN_CLUSTER
+
+        SHR AX, 4
+        JMP .ODD_CLUSTER
+
+.EVEN_CLUSTER:
+        AND AX, 0x0FFF
+
+.ODD_CLUSTER:
+        POP BX
+        POP ES
+        RET
+
+; AX <- LBA value.
+; CL <- Number of sectors to write.
+; DL <- Drive number.
+; ES:BX <- Pointer to buffer.
+WRITE_DISK:
+        PUSHA
+        PUSH ES
+
+        MOVZX DI, CL
+
+.WRITE_LOOP:
+        CALL LBA_TO_CHS
+        CALL WRITE_CHS
+        JC .RETURN
+
+        MOV SI, ES
+        ADD SI, 32
+        MOV ES, SI
+
+        INC AX
+        DEC DI
+        JNZ .WRITE_LOOP
+
+.RETURN:
+        POP ES
+        POPA
+        RET
+
+; ES:BX <- Pointer to buffer to be written.
+; CX[0:5] <- Sector number.
+; CX[6:15] <- Track/Cylinder.
+; DH <- Head number.
+; DL <- Drive number.
+WRITE_CHS:
+        PUSH DI
+        MOV DI, 3
+
+.READ_LOOP:
+        STC
+        PUSH AX
+        MOV AH, 0x03
+        MOV AL, 1
+        INT 0x13
+        POP AX
+        JNC .OUT
+
+        DEC DI
+        JNZ .READ_LOOP
+        STC
+
+.OUT:
+        POP DI
+        RET
+
+; AX <- LBA value.
+; CL <- Number of sectors to read.
+; DL <- Drive number.
+; ES:BX <- Pointer to buffer.
+READ_DISK:
+        PUSHA
+        PUSH ES
+
+        MOVZX DI, CL
+
+.READ_LOOP:
+        CALL LBA_TO_CHS
+        CALL READ_CHS
+        JC .RETURN
+
+        MOV SI, ES
+        ADD SI, 32
+        MOV ES, SI
+        INC AX
+        DEC DI
+        JNZ .READ_LOOP
+
+.RETURN:
+        POP ES
+        POPA
+        RET
+
+; ES:BX <- Pointer to target buffer.
+; CX[0:5] <- Sector number.
+; CX[6:15] <- Track/Cylinder.
+; DH <- Head number.
+; DL <- Drive number.
+READ_CHS:
+        PUSH DI
+        MOV DI, 3
+
+.READ_LOOP:
+        STC
+        PUSH AX
+        MOV AH, 0x02
+        MOV AL, 1
+        INT 0x13
+        POP AX
+        JNC .OUT
+
+        DEC DI
+        JNZ .READ_LOOP
+        STC
+
+.OUT:
+        POP DI
+        RET
+
+; AX <- LBA value.
+;
+; CX[0:5] -> Sector number.
+; CX[6:15] -> Track/Cylinder.
+; DH -> Head number.
+LBA_TO_CHS:
+        PUSH AX
+        PUSH DX
+
+        XOR DX, DX
+        DIV WORD[SECTORS_PER_TRACK]
+        INC DX
+        MOV CL, DL ; Get the sector number.
+
+        XOR DX, DX
+        DIV WORD[HEAD_COUNT]
+        SHL DX, 8 ; Get the head number.
+
+        MOV CH, AL
+        SHL AH, 6
+        OR CL, AH ; Get the number of tracks/cylinders.
+
+        MOV AL, DH
+        POP DX
+        MOV DH, AL
+        POP AX
+        RET
+
+; Floppy drives sometimes spit out an error if you don't reset after fast consecutive read/writes.
+RESET_DISK:
+        PUSH AX
+        PUSH DX
+
+        MOV AH, 0x00
+        MOV DL, BYTE[DRIVE_NUMBER]
+        INT 0x13
+
+        POP DX
+        POP AX
+        RET
+
+; AX <- Value.
+;
+; CL -> Result.
+LOG2:
+        PUSH AX
+
+        XOR CL, CL
+
+.LOOP:
+        SHR AX, 1
+
+        TEST AX, AX
+        JZ .OUT
+        INC CL
+        JMP .LOOP
+
+.OUT:
+        POP AX
+        RET
