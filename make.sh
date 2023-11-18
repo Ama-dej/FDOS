396cb27430a19fb562de5b23908e6609ee6e2807
Ama-dej <amadej.arh@gmail.com>
2023-11-18 20:01:40 +0100

    kaj so metaforično kadil ko so delal CGA?

--- make.sh
+++ make.sh
@@ -13,10 +13,11 @@ nasm -f bin prg/tetris.asm -o bin/games/tetris.bin
 nasm -f bin prg/big.asm -o bin/test_prg/a
 nasm -f bin prg/test.asm -o bin/test_prg/test.bin
 nasm -f bin prg/readfile.asm -o bin/test_prg/readfile.bin
-nasm -f bin prg/abc.asm -o bin/test_prg/abc.bin
+nasm -f bin prg/div.asm -o bin/test_prg/div.bin
 nasm -f bin prg/tobogan.asm -o bin/test_prg/tobogan.bin
 nasm -f bin prg/fib.asm -o bin/test_prg/fib.bin
 nasm -f bin prg/mines.asm -o bin/games/mines.bin
+nasm -f bin prg/mandel.asm -o bin/test_prg/mandel.bin
 
 dd if=/dev/zero of=img/floppy1440.img count=2880 bs=512
 mkfs.fat -F 12 img/floppy1440.img
---
+++ prg/div.asm
@@ -0,0 +1,68 @@
+[BITS 16]
+[ORG 0x0000]
+	
+	MOV AX, 1000
+	MOV BX, 3
+	CALL DIV
+
+	PUSH DX
+
+	MOV DX, AX
+	MOV AH, 0x03
+	INT 0x80
+
+	MOV AH, 0x0E
+	MOV AL, ' '
+	INT 0x10
+
+	MOV AH, 0x03
+	POP DX
+	INT 0x80
+
+	MOV AH, 0x0E
+	MOV AL, 0x0D
+	INT 0x10
+	MOV AL, 0x0A
+	INT 0x10
+
+	XOR AH, AH
+	INT 0x80
+
+; AX <- Divident
+; BX <- Divizor
+; DX <- Ostanek
+DIV:
+	PUSH BX
+	PUSH CX
+
+	XOR CX, CX
+
+.KO_VECJI:
+	CMP BX, AX
+	JA .ODSTEVAJ
+
+	ADD BX, BX
+	JMP .KO_VECJI
+
+.ODSTEVAJ:
+	CMP BX, AX
+	JA .ZAMAKNI
+
+	SUB AX, BX
+
+	INC CX
+
+.ZAMAKNI:
+	SHR BX, 1
+	JC .KONEC
+
+	SHL CX, 1
+	JMP .ODSTEVAJ
+
+.KONEC:
+	MOV DX, AX
+	MOV AX, CX
+
+	POP CX
+	POP BX
+	RET
