26bf7a5acdca15ecbe9e805f292871c53e83c4d3
Ama-dej <amadej.arh@gmail.com>
2023-12-03 14:56:08 +0100

    spremenu format izvršljivih programov in naredu make.sh pametnejš

--- make.sh
+++ make.sh
@@ -7,60 +7,41 @@ nasm -f bin src/boot.asm -o bin/boot.bin
 nasm -f bin src/dos.asm -o bin/dos.bin
 
 mkdir bin/games
-mkdir bin/test
-nasm -f bin prg/snake.asm -o bin/games/snake.prg
-nasm -f bin prg/tetris.asm -o bin/games/tetris.prg
-nasm -f bin prg/big.asm -o bin/test/velik.prg
-nasm -f bin prg/test.asm -o bin/test/test.bin
-nasm -f bin prg/readfile.asm -o bin/test/readfile.bin
-nasm -f bin prg/div.asm -o bin/test/div.prg
-nasm -f bin prg/tobogan.asm -o bin/test/tobogan.bin
-nasm -f bin prg/fib.asm -o bin/test/fib.prg
-nasm -f bin prg/mines.asm -o bin/games/mines.prg
-nasm -f bin prg/mandel.asm -o bin/test/mandel.prg
+mkdir bin/demo
+
+cd prg
+for file in *
+do
+	nasm -f bin "$file" -o ../bin/"${file%.*}.bin"
+done
+cd ..
+
+cd bin
+mv tetris.bin games/tetris.prg
+mv mines.bin games/mines.prg
+mv snake.bin games/snake.prg
+
+mv mandel.bin demo/mandel.prg
+mv fib.bin demo/fib.prg
+mv div.bin demo/div.prg
+cd ..
 
 dd if=/dev/zero of=img/floppy1440.img count=2880 bs=512
-mkfs.fat -F 12 img/floppy1440.img
 dd if=/dev/zero of=img/floppy720.img count=1440 bs=512
-mkfs.fat -F 12 img/floppy720.img
 dd if=/dev/zero of=img/floppy1200.img count=2400 bs=512
-mkfs.fat -F 12 img/floppy1200.img
 dd if=/dev/zero of=img/floppy360.img count=720 bs=512
-mkfs.fat -F 12 img/floppy360.img
 
-cp bin/boot.bin bin/boot1440.bin
-cp bin/boot.bin bin/boot720.bin
-cp bin/boot.bin bin/boot1200.bin
-cp bin/boot.bin bin/boot360.bin
-dd if=img/floppy1440.img of=bin/boot1440.bin bs=36 count=1 conv=notrunc
-dd if=img/floppy720.img of=bin/boot720.bin bs=36 count=1 conv=notrunc
-dd if=img/floppy1200.img of=bin/boot1200.bin bs=36 count=1 conv=notrunc
-dd if=img/floppy360.img of=bin/boot360.bin bs=36 count=1 conv=notrunc
+for img in img/*
+do
+	mkfs.fat -F 12 "$img"
 
-dd if=bin/boot1440.bin of=img/floppy1440.img conv=notrunc
-dd if=bin/boot720.bin of=img/floppy720.img conv=notrunc
-dd if=bin/boot1200.bin of=img/floppy1200.img conv=notrunc
-dd if=bin/boot360.bin of=img/floppy360.img conv=notrunc
+	num=$(echo "$img" | tr -dc '0-9')
 
-mcopy -i img/floppy1440.img bin/dos.bin "::DOS.SYS"
-mcopy -i img/floppy720.img bin/dos.bin "::DOS.SYS"
-mcopy -i img/floppy1200.img bin/dos.bin "::DOS.SYS"
-mcopy -i img/floppy360.img bin/dos.bin "::DOS.SYS"
+	cp bin/boot.bin "bin/boot$num.bin"
+	dd if="$img" of="bin/boot$num.bin" bs=36 count=1 conv=notrunc
+	dd if="bin/boot$num.bin" of="$img" conv=notrunc
 
-# mcopy -i img/floppy1440.img LICENSE "::LICENSE.TXT"
-# mcopy -i img/floppy720.img LICENSE "::LICENSE.TXT"
-# mcopy -i img/floppy1200.img LICENSE "::LICENSE.TXT"
-# mcopy -i img/floppy360.img LICENSE "::LICENSE.TXT"
-
-mcopy -i img/floppy1440.img bin/test "::TEST"
-mcopy -i img/floppy1440.img bin/games "::GAMES"
-mcopy -i img/floppy1440.img bin/games "::TEST/GAMES"
-
-mcopy -i img/floppy720.img bin/test "::TEST"
-mcopy -i img/floppy720.img bin/games "::GAMES"
-
-mcopy -i img/floppy1200.img bin/test "::TEST"
-mcopy -i img/floppy1200.img bin/games "::GAMES"
-
-mcopy -i img/floppy360.img bin/test "::TEST"
-mcopy -i img/floppy360.img bin/games "::GAMES"
+	mcopy -i "$img" bin/dos.bin "::DOS.SYS"
+	mcopy -i "$img" bin/demo "::DEMO"
+	mcopy -i "$img" bin/games "::GAMES"
+done
