de0f1b1123eda92fb2ffb20393a3ec53e8310601
Ama-dej <amadej.arh@gmail.com>
2024-01-11 20:35:03 +0100

    osnova lsdsk ukaza napisana

--- runvm.sh
+++ runvm.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 qemu-system-i386 -cpu 486 -fda $1
+# qemu-system-i386 -cpu 486 -drive file=img/floppy1440.img,index=0,if=floppy,format=raw -drive file=img/floppy720.img,index=1,if=floppy,format=raw
